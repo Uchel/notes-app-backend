4bf4fe4f243de58b45c26d6fe9101549efee3315
Uchel <russel.emilian@gmail.com>
2023-06-16 15:35:05 +0900

    nothing

--- src/server.js
+++ src/server.js
@@ -4,11 +4,11 @@ const routes = require("./routes");
 const init = async () => {
   const server = Hapi.server({
     port: 5000,
-    host:process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
-    routes:{
-        cors:{
-            origin:["*"],
-        },
+    host: process.env.NODE_ENV !== "production" ? "localhost" : "0.0.0.0",
+    routes: {
+      cors: {
+        origin: ["*"],
+      },
     },
   });
 
